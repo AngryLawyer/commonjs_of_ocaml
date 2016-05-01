06ad1258b9776993092fc57ec7ea62e538bd04ee
Tony Aldridge <zaragopha@hotmail.com>
2016-05-01 16:59:17 +0100

    Correctly match simple requires

--- lib/ppx/ppx.ml
+++ lib/ppx/ppx.ml
@@ -4,14 +4,25 @@ open Asttypes
 open Parsetree
 open Longident
 
+let make_require_string str =
+    Printf.sprintf "require('%s')" str
 
 let require_mapper argv =
   { default_mapper with
     expr = fun mapper expr ->
       match expr with
       | [%expr [%require [%e? str]]] ->
-          let args = [] in
-          Exp.apply ~loc:expr.pexp_loc (Exp.ident {txt = Longident.parse "ReactJS.create_element"; loc=expr.pexp_loc}) args
+          begin 
+              match str with
+              | { pexp_desc = Pexp_constant (Const_string (s, None)) } ->
+                  let req = make_require_string s in
+                  let args = [("", (Exp.constant (Const_string (req, None))))] in
+                  Exp.apply ~loc:expr.pexp_loc (Exp.ident {txt = Longident.parse "Js.Unsafe.js_expr"; loc=expr.pexp_loc}) args
+              | _ ->
+                raise (Location.Error (
+                    Location.error ~loc:expr.pexp_loc "[%require] expected a constant string"))
+          end
+
       | x -> default_mapper.expr mapper x;
   }
 
