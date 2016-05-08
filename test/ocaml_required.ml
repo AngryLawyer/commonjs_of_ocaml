4795b639e574c3ee23303fde22708801827dc3ea
Tony Aldridge <zaragopha@hotmail.com>
2016-05-08 15:07:42 +0100

    Test require_or_default too

--- test/ocaml_required.ml
+++ test/ocaml_required.ml
@@ -1,1 +1,8 @@
-CommonJS.export ([%require "./js_required"] * 2)
+let value =
+    let a = [%require "./js_required"] in
+    let b = [%require_or_default "./js_required" "6"] in
+    let c = [%require_or_default "./nonexistant" "7"] in
+    a + b + c
+
+let () =
+    CommonJS.export value
