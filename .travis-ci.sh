0b6a4333a8f1014365a06d6e94d3e3b824b3364b
Tony Aldridge <zaragopha@hotmail.com>
2016-05-08 15:12:25 +0100

    Actually include travis files

---
+++ .travis-ci.sh
@@ -0,0 +1,19 @@
+# Edit this for your own project dependencies
+OPAM_DEPENDS="ocamlfind js_of_ocaml"
+
+case "$OCAML_VERSION,$OPAM_VERSION" in
+4.02.3,1.2.2) ppa=avsm/ocaml42+opam12 ;;
+*) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
+esac
+
+echo "yes" | sudo add-apt-repository ppa:$ppa
+sudo apt-get update -qq
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+export OPAMYES=1
+opam init
+opam install ${OPAM_DEPENDS}
+eval `opam config env`
+
+ocaml setup.ml -configure --enable-tests
+ocaml setup.ml -build
+ocaml setup.ml -test
