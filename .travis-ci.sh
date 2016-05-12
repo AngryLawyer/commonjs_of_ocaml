ac93f20a4442e6089d5bc2dc6b369b5405f298c5
Tony Aldridge <tony@dabapps.com>
2016-05-12 09:17:38 +0100

    Attempt at getting a 4.03 build environment on Travis

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -3,6 +3,7 @@ OPAM_DEPENDS="ocamlfind js_of_ocaml"
 
 case "$OCAML_VERSION,$OPAM_VERSION" in
 4.02.3,1.2.2) ppa=avsm/ocaml42+opam12 ;;
+4.03.0,1.2.2) ppa=avsm/ocaml42+opam12 ;;
 *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
 esac
 
@@ -11,6 +12,11 @@ sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
 export OPAMYES=1
 opam init
+
+if [ $OCAML_VERSION = "4.03.0" ] then
+    opam switch 4.03.0+trunk
+fi
+
 opam install ${OPAM_DEPENDS}
 eval `opam config env`
 
